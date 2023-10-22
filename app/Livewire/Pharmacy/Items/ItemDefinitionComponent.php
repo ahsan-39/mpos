bf046cac4e1293a036b3359d504e59aed5d24ed2
Ahsan Rasheed <ahsanrasheed55@gmail.com>
2023-10-22 17:01:45 +0500

    item definiti

--- app/Livewire/Pharmacy/Items/ItemDefinitionComponent.php
+++ app/Livewire/Pharmacy/Items/ItemDefinitionComponent.php
@@ -2,10 +2,22 @@
 
 namespace App\Livewire\Pharmacy\Items;
 
+use App\Models\Pharmacy\ItemDefinition;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ItemDefinitionComponent extends Component
 {
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
+    public $currentPage;
+    public $lightBoxTitle = 'Add Sub Category';
+    public $hideRole = '';
+    public $perPage=10;
+    public $search=false;
+    public $updateMode = false;
+
     public function render()
     {
         return view('livewire.pharmacy.items.item-definition-component');
--- app/Models/Pharmacy/ItemDefinition.php
+++ app/Models/Pharmacy/ItemDefinition.php
@@ -4,17 +4,32 @@ namespace App\Models\Pharmacy;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Contracts\ActiveStatus;
 
-class ItemDefinition extends Model
+
+class ItemDefinition extends Model implements ActiveStatus
 {
     use HasFactory;
 
-    protected $fillable = ['item_code','is_expiry','generic_id','item_type_id','size_specification_id',
+    protected $fillable = ['is_expiry','generic_id','item_type_id','size_specification_id',
     'dosage_form_id','dosage_route_id','uom_id','strength_id','pack_size','unit_pack_size','is_active'];
 
     public function scopeActive($query)
     {
         return $query->where('is_active',true);
+    }
+
+    public function isActive(): bool
+    {
+        return $this->is_active;
+    }
+
+    public function markActive($active=true): ItemDefinition
+    {
+        $this->update([
+            'is_active' => $active
+        ]);
+        return $this;
     }
     
 }
